6e51a971c1dc645a16c9d30aafb61c1532279ef9
André Kouamé <kouameandre43@gmail.com>
2024-04-18 04:12:32 +0200

    init project

---
+++ src/main/java/com/nextu/kubernetesapp/KubernetesAppApplication.java
@@ -0,0 +1,13 @@
+package com.nextu.kubernetesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubernetesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KubernetesAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nextu/kubernetesapp/KubernetesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nextu.kubernetesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubernetesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
